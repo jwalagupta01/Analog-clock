234d9009e32c8cd70465c9c667239a7a1cb8457f
Jwala Gupta <jwalagupta58@gmail.com>
2025-08-22 12:57:47 +0530

    An analog wall clock with moving hands built using HTML, CSS, and JavaScript.

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("for test");        // for test only------------------->
+
+// html select 
+
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+
+
+// for rotate hands 
+setInterval(() =>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
